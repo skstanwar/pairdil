fa66d42332364aedd05afa619abd241368e53d93
Sanjay Kumar Saini <kumarsaini0098@gmail.com>
2025-06-17 14:54:17 +0000

    env files

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ import {sql} from "./config/superbase.js"
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 // (async () => {
 //   try {
